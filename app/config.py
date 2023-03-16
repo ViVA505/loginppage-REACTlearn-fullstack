7f7432658fd59e65b3d4292be6d48088e31270c4
ViVA505 <vladimirrezume5@gmail.com>
2023-03-16 03:34:19 +0300

    config-db

--- app/config.py
+++ app/config.py
@@ -5,6 +5,9 @@ from sqlmodel import SQLModel
 DB_CONFIG = f"postgresql+asyncpg://postgres:00@localhost:5432/test"
 
 #sdsdsdsds
+
+
+
 SECRET_KEY = "lemonsite12"
 ALGORITM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
