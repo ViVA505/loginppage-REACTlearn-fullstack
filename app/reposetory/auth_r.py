2b4e36a43ec522cbc041ec66f50434b3ea205416
ViVA505 <vladimirrezume5@gmail.com>
2023-03-16 03:18:49 +0300

    JWT token(auth_repo)

---
+++ app/reposetory/auth_r.py
@@ -0,0 +1,70 @@
+
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import jwt
+
+from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+
+from app.config import SECRET_KEY, ALGORITM
+
+
+
+
+#наследуем токен и дату
+class JWTRepo:
+
+    def __init__(self, data: dict = {}, token: str = None):
+        self.data = data
+        self.token = token
+
+#генерируем токен
+    def generate_token(self, expires_delta: Optional[timedelta] = None):
+        to_encode = self.data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITM)
+
+        return encode_jwt
+#декодем и чекаем как раз на корректность
+    def decode_token(self):
+        try:
+            decode_token = jwt.decode(
+                self.token, SECRET_KEY, algorithms=[ALGORITM])
+            return decode_token if decode_token["expires"] >= datetime.time() else None
+        except:
+            return {}
+
+    @staticmethod
+    def extract_token(token: str):
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITM])
+
+
+class JWTBearer(HTTPBearer):
+
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+#отвечаем на ошибки get запросов(403 ,  и так далее)
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(
+                    status_code=403, detail={"status": "Forbidden", "message": "Invalid authentication schema."})
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(
+                    status_code=403, detail={"status": "Forbidden", "message": "Invalid token or expired token."})
+            return credentials.credentials
+        else:
+            raise HTTPException(
+                status_code=403, detail={"status": "Forbidden", "message": "Invalid authorization code."})
+
+    @staticmethod
+    def verify_jwt(jwt_token: str):
+        return True if jwt.decode(jwt_token, SECRET_KEY, algorithms=[ALGORITM]) is not None else False
+
